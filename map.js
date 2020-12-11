7fa4888e4618eda15666bfcbc41bf1152e3afa4b
yusuf <yusufziyapyrz@gmail.com>
2020-12-11 20:36:34 +0300

    scuffed ring strings

--- map.js
+++ map.js
@@ -1,19 +1,57 @@
 //
 function createMap(gl) {
-    cube = new Cube(gl, 20);
-    cube2 = new Cube(gl, 35);
-    cube3 = new Cube(gl, 100);
-    cube4 = new Cube(gl, 50);
-    cylinder = new Cylinder(gl, 8, 30, 90);
-    cylinder.rotation.x = degToRad(30);
+    floor = new Cube(gl, 60);
+    ringPole1 = new Cube(gl, 5);
+    ringPole2 = new Cube(gl, 5);
+    ringPole3 = new Cube(gl, 5);
+    ringPole4 = new Cube(gl, 5);
+    ringString341 = new Cylinder(gl, 20, 2, 2);
+    ringString342 = new Cylinder(gl, 20, 1, 2);
+    ringString141 = new Cylinder(gl, 20, 2, 2);
+    ringString142 = new Cylinder(gl, 20, 2, 2);
+    ringString121 = new Cylinder(gl, 20, 2, 2);
+    ringString122 = new Cylinder(gl, 20, 2, 2);
+    wallBehind = new Cube(gl, 2);
 
-    cylinder.translation = {x: -20, y: 20, z: 60};
-    cube4.translation = {x: 0, y: -60, z: 10}
+    floor.translation = {x: 10, y: -50, z: 20}
+    floor.scale.x = 3;
+    floor.scale.z = 2;
+    
+    ringPole1.translation = {x: 95, y: -10, z: 75};
+    ringPole1.scale.y = 5;
 
-    cube.translation = {x: 20, y: -40, z: -70}
-    cube2.translation = {x:-40, y:-30, z:-150}
-    cube3.translation.y = 1000;
-    cube2.scale.y = 2;
+    ringPole2.translation = {x: -75, y: -10, z: 75};
+    ringPole2.scale.y = 5;
+
+    ringPole3.translation = {x: 95, y: -10, z: -35};
+     ringPole3.scale.y = 5;
+
+    ringPole4.translation = {x: -75, y: -10, z: -35};
+    ringPole4.scale.y = 5;
+
+    ringString341.translation = {x: -75, y: -13, z: 20};
+    ringString341.scale.z = 60;
+
+    ringString342.translation = {x: -75, y: -2, z: -40};
+    ringString342.scale.y = 60;
+    ringString342.rotation.x = degToRad(90);
+
+    ringString141.translation = {x: 10, y: -13, z: 75};
+    ringString141.scale.x = 90;
+
+    ringString142.translation = {x: 10, y: -7, z: 75};
+    ringString142.scale.x = 90;
+
+    ringString121.translation = {x: 95, y: -13, z: 20};
+    ringString121.scale.z = 60;
+
+    ringString122.translation = {x: 95, y: -2, z: 20};
+    ringString122.scale.z = 60;
+
+    wallBehind.translation = {x: 10, y: 0, z: -40};
+    wallBehind.scale.y = 20;
+    wallBehind.scale.x = 90;
+
 }
 
 /**
@@ -23,6 +61,5 @@ function createMap(gl) {
 function updateObjects(delta) {
     let rotationSpeed = degToRad(17); // radian per second
 
-    cube2.rotation.y += rotationSpeed * delta;
-    cube2.rotation.z += rotationSpeed * delta;
+
 }
